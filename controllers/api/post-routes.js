62204a55daff449f068cbcdc98faae19e974b76b
Billy Dorman <b.d.dorman12@gmail.com>
2021-08-03 18:06:40 -0500

    work on dashboard-routes and api controllers

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_content',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
